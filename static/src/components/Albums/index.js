5c3caa49f128672107a123d5bfd616140599755c
Agne Klimaite <agnaite@me.com>
2017-07-20 12:07:39 -0700

    added react gallery

--- static/src/components/Albums/index.js
+++ static/src/components/Albums/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
 
-export const Albums = () =>
-    <section>
-        <div className="container text-center">
-          Albums
-        </div>
-    </section>;
+export class Albums extends Component {
+    render() {
+        return (
+          <div className="container">
+            <Gallery photos={PHOTO_SET} onClickPhoto={this.openLightbox}/>
+          </div>
+        );
+    }
+}
+
+const PHOTO_SET = [
+  {
+    src: '../images/1.jpg',
+    sizes:[
+      '(min-width: 480px) 50vw',
+      '(min-width: 1024px) 33.3vw',
+      '100vw'
+    ],
+    width: 681,
+    height: 1024,
+    alt: 'image 1',
+  },
+  {
+    src: '../images/2.jpg',
+    sizes:[
+      '(min-width: 480px) 50vw',
+      '(min-width: 1024px) 33.3vw',
+      '100vw'
+    ],
+    width: 681,
+    height: 1024,
+    alt: 'image 1',
+  },
+  {
+    src: '../images/3.jpg',
+    sizes:[
+      '(min-width: 480px) 50vw',
+      '(min-width: 1024px) 33.3vw',
+      '100vw'
+    ],
+    width: 681,
+    height: 1024,
+    alt: 'image 1',
+  }
+];
